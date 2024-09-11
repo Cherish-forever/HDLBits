3ae2284f9b583c06ed65b774f3c39c974d755765
Weiwei Guo <guoweiwei@syriusrobotics.com>
2024-09-11 10:07:48 +0800

    basic: SimpleWire: Add simple wire verilog and python script

---
+++ 2.VerilogLanguage/1.Basics/1.SimpleWire/python/SimpleWire.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from migen import *
+from migen.fhdl import verilog
+
+class SimpleWire(Module):
+    def __init__(self):
+        self.out = Signal()
+        self.in_ = Signal()
+        self.comb += self.out.eq(self.in_)
+
+
+if __name__ == "__main__":
+    top = SimpleWire();
+    print(verilog.convert(top, ios={top.out, top.in_}))
