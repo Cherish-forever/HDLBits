3527eae5c567d6cc27610580b09f960131a35ad2
Weiwei Guo <guoweiwei@syriusrobotics.com>
2024-10-22 18:39:13 +0800

    Circuits: SequentialLogic: Add shift register verilog file

---
+++ 3.Circuits/2.SequentialLogic/3.ShifRegisters/verilog/8.ShiftRegister.v
@@ -0,0 +1,16 @@
+module top_module (
+		   input [3:0] 	SW,
+		   input [3:0] 	KEY,
+		   output [3:0] LEDR
+		   ); //
+
+   always @(posedge KEY[0]) begin
+      if (KEY[2])
+	LEDR <= SW;
+      else if (KEY[1])
+	LEDR <= { KEY[3], LEDR[3:1] };
+      else
+	LEDR <= LEDR;
+   end
+
+endmodule
